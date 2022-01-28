98e324551b043e1328d0233897696c343f065cb6
Nkashyap-anand <55659236+Nkashyap-anand@users.noreply.github.com>
2022-01-28 11:43:44 +0530

    steps completed for heroku

--- weatherApp/weatherApp/settings.py
+++ weatherApp/weatherApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,6 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
